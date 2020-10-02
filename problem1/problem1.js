f1b33c2034232e448fb463735a21f97e7d03806b
EduardoMontanha <edumontanha14@gmail.com>
2020-10-01 23:43:17 -0300

    Create code to solve the problem

---
+++ problem1/problem1.js
@@ -0,0 +1,30 @@
+const form = document.getElementById("form");
+const listInput = document.getElementById("list");
+const numInput = document.getElementById("num");
+const outputInput = document.getElementById("output");
+
+
+form.onsubmit = event => {
+    event.preventDefault();
+
+    //Get values
+    let list = listInput.value.replace(/\s+/gi, "").split(",");
+    let number = parseInt(numInput.value);
+
+    //Transform text on number
+    list = list.map(num => parseInt(num));
+
+    for(let i = 0; i < list.length; i++) {
+        for(let j = 0; j < list.length; j++) {
+            if (j !== i) {
+                if (list[i] + list[j] === number) {
+                    outputInput.value = "TRUE";
+                    return;
+                } else {
+                    outputInput.value = "FALSE";
+                }
+            }
+        }
+    }
+
+}
