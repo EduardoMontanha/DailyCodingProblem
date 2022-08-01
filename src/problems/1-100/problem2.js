6f2e5d01172c0a7dba0688959a1741859383bd5f
EduardoMontanha <edumontanha14@gmail.com>
2022-08-01 13:59:31 -0300

    Mock problems

---
+++ src/problems/1-100/problem2.js
@@ -0,0 +1,27 @@
+
+
+const Problem2 = () => {
+
+    return (
+        <div className="problem-wrap">
+            <section className="problem-header">
+                <h1>Problem #2</h1>
+
+                <p>This problem was recently asked by <strong>Google</strong>.</p>
+                <p>
+                    Given a list of numbers and a number <code>k</code>, return whether
+                    any two numbers from the list add up to <code>k</code>.
+                </p>
+                <p>
+                    For example, given <code>[10, 15, 3, 7]</code> and <code>k</code> of
+                    <code>17</code>, return true since <code>10 + 7</code> is <code>17</code>.
+                </p>
+                <p>Bonus: Can you do this in one pass?</p>
+            </section>
+
+            <section className="problem-input-output"></section>
+        </div>
+    );
+}
+
+export default Problem2;
