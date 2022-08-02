0be01779b4dc392c9c8b5cdbe36224138bcdd6dc
EduardoMontanha <edumontanha14@gmail.com>
2022-08-01 23:21:26 -0300

    Added years

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,27 @@
+import { Link } from 'react-router-dom';
+import Header from "../components/Header";
 
 
 const Home = () => {
 
     return (
-        <main>
-            <ul>
-                <li>test</li>
-                <li>test 2</li>
-            </ul>
-        </main>
+        <>
+            <Header />
+
+            <main className='home'>
+                <ul>
+                    <li>
+                        <Link to="/2022">2022</Link>
+                    </li>
+                    <li>
+                        <Link to="/2021">2021</Link>
+                    </li>
+                    <li>
+                        <Link to="/2020">2020</Link>
+                    </li>
+                </ul>
+            </main>
+        </>
     );
 }
 
