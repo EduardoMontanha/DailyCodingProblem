c14be54c2d6de01762e2b18cee6262960a9011f5
EduardoMontanha <edumontanha14@gmail.com>
2022-08-01 23:20:15 -0300

    Create home header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,22 @@
+import logo from '../assets/images/logo.svg';
 
 
 const Header = () => {
 
     return(
-        <header>
-            
+        <header className='header'>
+            <img src={logo} alt="Logo" />
+
+            <div>
+                <h1>Daily Coding Problem</h1>
+
+                <h2>
+                    Problems given by&nbsp;
+                    <a href='https://www.dailycodingproblem.com/' target="_blank" rel="noreferrer">
+                        dailycodingproblem.com
+                    </a>
+                </h2>
+            </div>
         </header>
     );
 }
