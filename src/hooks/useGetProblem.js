a0f703b41481d34104fd851c6bff89415c7efb6c
EduardoMontanha <edumontanha14@gmail.com>
2022-08-03 11:54:50 -0300

    Change Problem to use new hook

---
+++ src/hooks/useGetProblem.js
@@ -0,0 +1,19 @@
+import Problems100 from '../problems/1-100';
+import Problems200 from '../problems/101-200';
+
+
+const useGetProblem = (id) => {
+
+    const intId = parseInt(id);
+    let ProblemComponent = undefined;
+
+    if (intId <= 100) {
+        ProblemComponent = Problems100[intId];
+    } else if (intId <= 200) {
+        ProblemComponent = Problems200[intId];
+    }
+
+    return <ProblemComponent />;
+}
+
+export default useGetProblem;
--- src/pages/Problem.js
+++ src/pages/Problem.js
@@ -1,29 +1,17 @@
-import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import useGetProblem from '../hooks/useGetProblem';
 import Return from '../components/Return';
-import Problems100 from '../problems/1-100';
-import Problems200 from '../problems/101-200';
 
 
 const Problem = () => {
     const { id } = useParams();
-    const [problem, setProblem] = useState();
+    const problem = useGetProblem(id);
     
-    useEffect(() => {
-        const intId = parseInt(id);
-
-        if (intId <= 100) {
-            setProblem(Problems100[intId]);
-        } else if (intId <= 200) {
-            setProblem(Problems200[intId]);
-        }
-    }, []);
-
     return (
         <>
             <Return />
 
-            <main className="problem-wrap">
+            <main className="problem">
                 {problem}
             </main>
         </>
