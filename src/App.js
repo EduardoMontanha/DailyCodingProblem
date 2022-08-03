ae1160454903fc4c4cef6445f50ca44fac2db722
EduardoMontanha <edumontanha14@gmail.com>
2022-08-03 15:58:36 -0300

    Added footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { BrowserRouter } from 'react-router-dom';
+import Footer from './components/Footer';
 import Router from './Router';
 
 function App() {
     return (
         <BrowserRouter>
             <Router />
+            <Footer />
         </BrowserRouter>
     );
 }
