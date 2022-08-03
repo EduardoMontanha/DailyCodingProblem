0529a18c9261446a0fa80699948948a2b3c4126e
EduardoMontanha <edumontanha14@gmail.com>
2022-08-03 13:23:00 -0300

    Finish logic to display months and problems

--- src/pages/Year.js
+++ src/pages/Year.js
@@ -1,14 +1,24 @@
-import { useEffect } from "react";
-import { useParams } from 'react-router-dom';
+import { useEffect, useState } from "react";
+import { Link, useParams } from 'react-router-dom';
 import Return from "../components/Return";
 
 
 const Year = () => {
     const { year } = useParams();
+    const [yearData, setYearData] = useState();
 
     useEffect(() => {
-        
-    });
+        const intYear = parseInt(year);
+
+        if (intYear === 2020) {
+            setYearData(require("../assets/data/2020.json"));
+        } else if (intYear === 2021) {
+            // setYearData(require("../assets/data/2021.json"));
+        } else if (intYear === 2022) {
+            // setYearData(require("../assets/data/2022.json"));
+        }
+
+    }, [year]);
 
     return (
         <>
@@ -16,6 +26,48 @@ const Year = () => {
 
             <main>
                 <h1>{year}</h1>
+
+                <section className="months">
+                    {!!yearData ? 
+                        Object.keys(yearData).slice(0).reverse().map((month, key) => {
+                            const monthData = yearData[month];
+                            let amountSolved = 0;
+
+                            for (let i = 0; i < monthData.length; i++) {
+                                const problem = monthData[i];
+
+                                if (problem.solved || !problem.canBeSolved) {
+                                    amountSolved++;
+                                }
+                            }
+
+                            return (
+                                <div className="card" key={key}>
+                                    <div className="card-header">
+                                        <h2>{month}, {year}</h2>
+                                        <h3>{amountSolved}/{monthData.length}</h3>
+                                    </div>
+
+                                    <div className="card-body">
+                                        <ol reversed>
+                                            {monthData.slice(0).reverse().map((problem, problemKey) => {
+                                                return (
+                                                    <li key={problemKey}>
+                                                        <Link to={`/problem/${problem.id}`}>
+                                                            Problem #{problem.id} - {problem.askedBy} - {problem.difficulty}
+                                                        </Link>
+                                                    </li>
+                                                );
+                                            })}
+                                        </ol>
+                                    </div>
+                                </div>
+                            );
+                        })
+                        :
+                        <p>Nada</p>
+                    }
+                </section>
             </main>
         </>
     );
